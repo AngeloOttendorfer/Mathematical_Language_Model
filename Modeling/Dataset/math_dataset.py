7c4c926375dedf82d372e42d987a648ff2b1ab8c
AngeloOttendorfer <angelo.ottendorfer@gmx.at>
2024-01-30 15:19:02 +0100

    Initial commit

---
+++ Modeling/Dataset/math_dataset.py
@@ -0,0 +1,122 @@
+import os
+import random
+import time
+
+import torch
+import torch.nn.functional as F
+
+
+class MathDataset(torch.utils.data.Dataset):
+    def __init__(self, math_dataroot, tokenizer, mode, max_tokens, sample_inputs=None, encodings=None, packing=None,
+                 randomize=None, pack_end=None, clean_numbers=True, peek_fraction=(0.1, 1.0)):
+        self.math_dataroot = math_dataroot
+        self.tokenizer = tokenizer
+        self.mode = mode
+        self.max_tokens = max_tokens
+        self.sample_inputs = sample_inputs
+        self.encodings = encodings
+        self.packing = packing
+        self.randomize = randomize
+        self.pack_end = pack_end
+        self.clean_numbers = clean_numbers
+        self.peek_fraction = peek_fraction
+
+        if self.mode in {'gpt2', 'bert-base-uncased', 't5-base-uncased'}:
+            self.clean_sample = self.clean_filter_sample
+            self.packing = True
+            self.randomize = True
+            self.include_fnames = False
+            self.pack_end = True
+        elif self.mode in {'gpt2-eval', 'bert-base-uncased-eval', 't5-base-uncased'}:
+            self.clean_sample = self.clean_filter_sample_eval
+            self.packing = True
+            self.randomize = False
+            self.include_fnames = True
+            self.pack_end = True
+
+        self.bad_fnames = set()
+        self.i = 0
+
+    def __len__(self):
+        raise NotImplementedError()
+
+    def __getitem__(self, index):
+        random.seed(os.getpid() + time.time() + random.random())
+        if self.model in {'gpt2'}:
+            curr_input_ids = []
+            curr_label_ids = []
+            curr_fnames = []
+            num_samples = 0
+            while len(curr_input_ids) + 1 <= self.max_tokens and len(curr_label_ids) + 1 <= self.max_tokens:
+                curr_sample, fname = self.get_random_sample()
+                if curr_sample is None:
+                    return {
+                        "input_ids": torch.zeros([self.max_tokens]),
+                        "labels": torch.zeros([self.max_tokens]),
+                        "fnames": [fname]
+                    }
+                if not self.pack_end and (
+                        (len(curr_input_ids) + 1 + len(curr_sample['input_ids_list']) > self.max_tokens) or
+                        (len(curr_label_ids) + 1 + len(curr_sample['label_ids_list']) > self.max_tokens)):
+                    break
+
+                curr_input_ids.extend(curr_sample['input_ids_list'])
+                curr_label_ids.extend(curr_sample['label_ids_list'])
+                curr_fnames.append(fname)
+
+                num_samples += 1
+
+                if not self.packing:
+                    break
+
+                input_ids = torch.LongTensor(curr_input_ids)
+                label_ids = torch.LongTensor(curr_label_ids)
+                input_ids = input_ids[:self.max_tokens]
+                label_ids = label_ids[:self.max_tokens]
+
+                if len(curr_input_ids) < self.max_tokens and 'eval' not in self.mode:
+                    num_to_pad = self.max_tokens - len(curr_input_ids)
+                    input_ids = F.pad(input_ids, [0, num_to_pad], mode='constant', value=self.tokenizer.pad_token_id)
+                if len(curr_label_ids) < self.max_tokens and 'eval' not in self.mode:
+                    num_to_pad = self.max_tokens - len(curr_label_ids)
+                    label_ids = F.pad(label_ids, [0, num_to_pad], mode='constant', value=-100)
+
+                if self.include_fnames:
+                    return {
+                        "input_ids": input_ids,
+                        "labels": label_ids,
+                        "fnames": curr_fnames
+                    }
+                else:
+                    return {
+                        "input_ids": input_ids,
+                        "labels": label_ids
+                    }
+
+        elif self.mode in {'bert-base-uncased', 't5-base-uncased'}:
+            input_ids = self.encodings['input_ids'][index]
+            labels = self.encodings['labels'][index]
+            attention_mask = self.encodings['attention_mask'][index]
+            token_type_ids = self.encodings['token_type_ids'][index]
+            return {
+                'input_ids': input_ids,
+                'labels': labels,
+                'attention_mask': attention_mask,
+                'token_type_ids': token_type_ids if self.mode in {'bert-base-uncased'} else None
+            }
+
+    def get_random_sample(self):
+        random_sample = None
+        while random_sample is None:
+            if self.randomize:
+                q, a, fname = random.choice(self.samples)
+            else:
+                q, a, fname = self.samples[self.i]
+                self.i = (self.i + 1) % len(self.samples)
+
+            random_sample = self.clean_sample(q, a)
+
+            if not self.randomize:
+                break
+
+        return random_sample, fname
---
+++ Modeling/Dataset/sample_tokenizer.py
@@ -0,0 +1,105 @@
+import os
+
+import torch
+from tqdm import tqdm
+
+from math_dataset import MathDataset
+
+
+class SampleTokenizer(MathDataset):
+    def __len__(self):
+        return int(len(self.samples))
+
+    def initialize(self):
+        with open(self.dataroot, 'r') as fp:
+            all_filenames = fp.readlines()
+
+        print(f"{self.__class__.__name__}: Loading samples from {len(all_filenames)}")
+        samples_raw = []
+        for fname in tqdm(all_filenames):
+            fname = fname.strip()
+            print(fname)
+
+            if not os.path.isfile(fname):
+                print(f"Skipping {fname}")
+                continue
+            with open(fname, 'r') as fp:
+                question = ""
+                answers = []
+                reading_question = True
+                curr_section = ""
+                for line in fp:
+                    if line == "Problem:\n":
+                        reading_question = True
+                    elif line == "Answer:\n":
+                        if reading_question:
+                            question = curr_section
+                        else:
+                            answers.append(curr_section)
+                        curr_section = ""
+                        reading_question = False
+                    else:
+                        curr_section += line
+
+                for a in answers:
+                    samples_raw.append((question, a, fname))
+
+            self.samples = samples_raw
+            del samples_raw
+
+            print(f"{self.__class__.__name__}: Loaded {len(self.samples)} samples.")
+
+    def clean_filter_sample(self, sample):
+        if sample == None:
+            return None
+
+        question, answer = sample
+
+        if self.clean_numbers:
+            question = _clean_numbers(question)
+            answer = _clean_numbers(answer)
+
+        answer_final = last_boxed_only_string(answer)
+
+        question_ids = torch.LongTensor(self.tokenizer.encode("\nQUESTION:\n" + question, verbose=False))
+        sep_ids = torch.LongTensor(self.tokenizer.encode("\nFULL SOLUTION.\n", verbose=False))
+        answer_ids = self.tokenizer.encode(answer, verbose=False)
+        answer_ids.append(self.tokenizer.eos_token_id)
+
+        if self.mode in {'t5-base-uncased'}:
+            answer_ids = answer_ids['input_ids']
+            answer_ids[answer_ids == self.tokenizer.pad_token_id] = -100
+
+        answer_ids = torch.LongTensor(answer_ids)
+
+        inputs = torch.cat([
+            question_ids,
+            sep_ids,
+            answer_ids
+        ], dim=0)
+
+        label_ids = torch.cat([
+            torch.ones_like(question_ids) * -100,
+            torch.ones_like(sep_ids) * -100,
+            answer_ids.clone()
+        ], dim=0)
+
+        if self.mode in {'bert-base-uncased'}:
+            random_tensor = torch.rand(inputs['input_ids'].shape)
+            masked_tensor = (random_tensor < 0.15) * (inputs['input_ids'] != 101) * (inputs['input_ids'] != 102) * (
+                        inputs['input_ids'] != 0)
+            nonzero_indices = []
+            for i in range(len(masked_tensor)):
+                nonzero_indices.append(torch.flatten(masked_tensor[i].nonzero()).tolist())
+            for i in range(len(inputs['input_ids'])):
+                inputs['input_ids'] = inputs['input_ids'][i, nonzero_indices[i]] != 103
+
+        input_ids = inputs['input_ids'].tolist()
+        label_ids = label_ids.tolist()
+
+        return {
+            'input_ids_list': input_ids,
+            'label_ids_list': label_ids,
+            'attention_mask_list': inputs['attention_mask'] if self.mode in {'bert-base-uncased', 't5-base-uncased'} else None
+        }
+            
